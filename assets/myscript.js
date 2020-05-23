48bcfe20640a534a9a0b068dd6fb1895ec262721
Shororo <56631085+Shororo@users.noreply.github.com>
2020-05-24 01:46:32 +0900

    Add files via upload

--- assets/myscript.js
+++ assets/myscript.js
@@ -36,6 +36,47 @@ $(window).resize(function() {
    }
  });
 	//スマホ用navここまで
+
+
+  //スムーススクロール
+    $(document).ready(function(){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+  //a要素をクリックしたら
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //スクロールしたくないa要素は以下のif文にハッシュを入力してスクロール実行を阻止しよう
+    if(href != '#carouselExampleControls'){
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  }
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    $("nav").slideUp();
+    $(".nav-cover").slideUp();
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({scrollTop:position}, 500);
+  }
+});
+
 	
 } else {
   //720pxより大きい時の処理
@@ -66,5 +107,43 @@ $(window).resize(function() {
     });
 
   }
+
+
+  //スムーススクロール
+    $(document).ready(function(){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+  //a要素をクリックしたら
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //スクロールしたくないa要素は以下のif文にハッシュを入力してスクロール実行を阻止しよう
+    if(href != '#carouselExampleControls'){
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  }
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({scrollTop:position}, 500);
+  }
+});
 
   });
